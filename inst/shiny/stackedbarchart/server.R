81bdfb91324ae5293d23d0b34daf7a36af5a3f71
Christian Panse <cp@fgcz.ethz.ch>
2020-06-16 11:43:27 +0200

    working

--- inst/shiny/stackedbarchart/server.R
+++ inst/shiny/stackedbarchart/server.R
@@ -6,130 +6,162 @@
 #
 #    http://shiny.rstudio.com/
 #
-
+library(readr)
 library(shiny)
 library(ggplot2)
 library(DT)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    
+  
   output$selectRData <- renderUI({
-    selectInput("rdata", "rdata", c('uvpd.20200522.RData','uvpd.20200612.RData'), multiple = FALSE, selected = 'uvpd.20200612.RData')
+    selectInput("rdata", "rdata", c('uvpd.20200522.RData', 'uvpd.20200612.RData'),
+                 multiple = FALSE, selected = 'uvpd.20200612.RData')
   })
   
-    getData0 <- reactive({
-        e <- new.env()
-        load(input$rdata, envir = e)
-        return(e$X.top3.master.intensity)
-    })
-    
-    getData <- reactive({
-        load("uvpd.20200522.RData")
-        
-        
-        X <- getData0()
-        Y <- do.call('rbind', do.call('rbind', X.top3.master.intensity.MS2))
-    
-        X$m <- paste(X$file, X$scan)
-        Y$m <- paste(Y$file, Y$scan)
-        XY <- base::merge(X, Y, by="m")
-       
-        XY$fragmode <- gsub("uvpd50.00", "uvpd050.00", XY$fragmode)
-        XY$fragmode <- gsub("uvpd25.00", "uvpd025.00", XY$fragmode)
-        XY
-        
-    })
-    
-    getFilteredData <- reactive({
-        DF <- getData()
-        
-        DF[DF$compound %in% input$compound & DF$ppmerror < as.numeric(input$ppmerror), ]
-    })
-    
-    getAggregatedData <- reactive({
-        aggregate(intensity ~ mZ * file.y * fragmode * compound * formula * Group * mode, data=getFilteredData(), FUN=sum)
-    })
-    
-    getLevels <- reactive({
-        DF <- getData()
-        sort(unique(DF$fragmode))
-    })
-    
-    getCompound <-  reactive({
-        message("getCompound")
-        DF <- getData()
-        rv <- sort(unique(DF$compound))
-        message(rv)
-        rv
-    })
-     
-    output$selectCompound <- renderUI({
-        selectInput("compound", "compound", getCompound(), multiple = FALSE, selected = "Triadimenol")
-    })
-    
-    output$distPlot <- renderPlot({
-      par(mfrow=c(1, 2))
-        hist(getFilteredData()$ppmerror, main=input$ppmerror)
-        hist(getFilteredData()$eps)
-        return
-    })
 
-    getTable <- reactive({
-        DF <- getAggregatedData()
-        print(names(DF))
-        rv <- table(unique(subset(DF, select=c('fragmode','formula')))[,1])
-        
-       rv <- (as.data.frame(rv))
-    })
+  getData <- reactive({
+    e <- new.env()
     
-    output$table <- renderTable({
-        
-        getTable()
-    })
-
-    output$stackedBarChart <- renderPlot({
-       
-        gp <- ggplot(data = getAggregatedData(),
-                     aes(x = factor(fragmode, levels = getLevels()),
-                         y = log(intensity, 10),
-                         fill=reorder(formula, mZ))) +
-            geom_bar(stat="identity", position = position_stack(reverse = FALSE)) +
-            scale_x_discrete(drop=FALSE) +
-            theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-            facet_wrap(~ compound * Group * mode, scales="free", drop=FALSE)
-        
-        # gp2 <- ggplot(data=unique(subset(S, select=c('fragmode','formula'))), aes(x=factor(fragmode, levels = sort(unique(DF$fragmode))), fill=(formula))) + ggplot2::geom_bar()
-        gp
-    })
-    output$xyplot <- renderPlot({
-        lattice::xyplot(intensity ~ mZ | fragmode, 
-                        group=paste(file.y, scan.y),
-                        scales = list(y="free"),
-                        auto.key =list(space = "right"),
-                        data=getFilteredData(),
-                        type='h', layout = c(1,13))
-    }, width=1000, height=1000)
-    
-    output$bwplot <- renderPlot({
+    fn <- file.path(system.file(package = 'uvpd'), "extdata", input$rdata)
    
-        lattice::bwplot(eps ~  fragmode, 
-                        data= getFilteredData(),
-                        scales = list(x = list(rot = 45)))
-    }, height=250)
- 
-    output$barchart <- renderPlot({
-        lattice::barchart(log(intensity) ~ fragmode | compound,
-                          group=mode ,
-                          data=aggregate(intensity ~ mode * fragmode * compound, data=getFilteredData(), FUN=sum),
-                          scales = list(x = list(rot = 45)),
-                          layout=c(6,7))
-    }, width=1000, height=1000)
+    load(fn, envir = e)
+    X<- e$X.top3.master.intensity
+    Y <- Y <- do.call('rbind', do.call('rbind',  e$X.top3.master.intensity.MS2))
     
-    output$top3 <- renderPlot({
-      lattice::xyplot(tic ~ master.intensity | fragmode * Compound,
-           #subset = Compound == "Triadimenol",
-           group=fragmode,
-           data=getFilteredData())
-    }, width=1000)
+    X$m <- paste(X$file, X$scan)
+    Y$m <- paste(Y$file, Y$scan)
+    XY <- base::merge(X, Y, by="m")
+    
+    XY$fragmode <- gsub("uvpd50.00", "uvpd050.00", XY$fragmode)
+    XY$fragmode <- gsub("uvpd25.00", "uvpd025.00", XY$fragmode)
+    XY
+  })
+  
+  
+  getThermoUVPD_feb2019 <- reactive({
+    
+    fn <- file.path(system.file(package = 'uvpd'), "extdata/ThermoUVPD_feb2019.csv")
+    ThermoUVPD_feb2019 <- read_csv(fn, na = c("", "#N/A"))
+    
+    ThermoUVPD_feb2019[ThermoUVPD_feb2019$Compound %in% getData()$Compound,
+                       c("Compound", "Bruto formula", "Cluster number", "Group")]
+  })
+  
+  
+  
+  getFilteredData <- reactive({
+    DF <- getData()
+    
+    DF[DF$compound %in% input$compound & DF$ppmerror < as.numeric(input$ppmerror), ]
+  })
+  
+  getAggregatedData <- reactive({
+    aggregate(intensity ~ mZ * file.y * fragmode * compound * formula * Group * mode,
+              data=getFilteredData(), FUN=sum)
+  })
+  
+  getLevels <- reactive({
+    DF <- getData()
+    sort(unique(DF$fragmode))
+  })
+  
+  getCompound <-  reactive({
+    message("getCompound")
+    DF <- getData()
+    rv <- sort(unique(DF$compound))
+    message(rv)
+    rv
+  })
+  
+  output$selectCompound <- renderUI({
+    selectInput("compound", "compound", getCompound(), multiple = FALSE, selected = "Triadimenol")
+  })
+  
+  output$distPlot <- renderPlot({
+    par(mfrow=c(1, 2))
+    hist(getFilteredData()$ppmerror, main=input$ppmerror)
+    hist(getFilteredData()$eps)
+    return
+  })
+  
+  
+  getTable <- reactive({
+    DF <- getAggregatedData()
+    print(names(DF))
+    rv <- table(unique(subset(DF, select=c('fragmode','formula')))[,1])
+    
+    rv <- (as.data.frame(rv))
+  })
+  
+  output$ThermoUVPD <- renderTable({
+    getThermoUVPD_feb2019()  
+  })
+  
+  output$table <- renderTable({
+    
+    getTable()
+  })
+  
+  output$stackedBarChart <- renderPlot({
+    
+    gp <- ggplot(data = getAggregatedData(),
+                 aes(x = factor(fragmode, levels = getLevels()),
+                     y = log(intensity, 10),
+                     fill=reorder(formula, mZ))) +
+      geom_bar(stat="identity", position = position_stack(reverse = FALSE)) +
+      scale_x_discrete(drop=FALSE) +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+      facet_wrap(~ compound * Group * mode, scales="free", drop=FALSE)
+    
+    # gp2 <- ggplot(data=unique(subset(S, select=c('fragmode','formula'))), aes(x=factor(fragmode, levels = sort(unique(DF$fragmode))), fill=(formula))) + ggplot2::geom_bar()
+    gp
+  },width=1000,height = 400)
+  
+  output$stackedBarChartGroup <- renderPlot({
+    
+    gp <- ggplot(data = getAggregatedData(),
+                 aes(x = factor(fragmode, levels = getLevels()),
+                     y = log(intensity, 10),
+                     fill=reorder(formula, mZ))) +
+      geom_bar(stat="identity", position = position_stack(reverse = FALSE)) +
+      scale_x_discrete(drop=FALSE) +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+      facet_wrap(~ compound * Group * mode, scales="free", drop=FALSE)
+    
+    # gp2 <- ggplot(data=unique(subset(S, select=c('fragmode','formula'))), aes(x=factor(fragmode, levels = sort(unique(DF$fragmode))), fill=(formula))) + ggplot2::geom_bar()
+    gp
+  },width=1000,height = 400)
+  
+  
+  output$xyplot <- renderPlot({
+    lattice::xyplot(intensity ~ mZ | fragmode, 
+                    group=paste(file.y, scan.y),
+                    scales = list(y="free"),
+                    auto.key =list(space = "right"),
+                    data=getFilteredData(),
+                    type='h', layout = c(1,13))
+  }, width=1000, height=1000)
+  
+  output$bwplot <- renderPlot({
+    
+    lattice::bwplot(eps ~  fragmode, 
+                    data= getFilteredData(),
+                    scales = list(x = list(rot = 45)))
+  }, width=1000, height=200)
+  
+  output$barchart <- renderPlot({
+    lattice::barchart(log(intensity) ~ fragmode | compound,
+                      group=mode ,
+                      data=aggregate(intensity ~ mode * fragmode * compound, data=getFilteredData(), FUN=sum),
+                      scales = list(x = list(rot = 45)),
+                      layout=c(6,7))
+  }, width=1000, height=1000)
+  
+  output$top3 <- renderPlot({
+    lattice::xyplot(tic ~ master.intensity | fragmode * Compound,
+                    #subset = Compound == "Triadimenol",
+                    group=fragmode,
+                    data=getFilteredData())
+  }, width=1000, height = 400)
 })
--- inst/shiny/stackedbarchart/ui.R
+++ inst/shiny/stackedbarchart/ui.R
@@ -11,33 +11,49 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
+    
     # Application title
     titlePanel("Ultra HRMS in combination with UVPD fragments"),
-
+    
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             tagList(
                 htmlOutput('selectRData'),
-            htmlOutput('selectCompound'),
-            selectInput("ppmerror", "ppm error cut-off",c(1, 5, 10, 15, 20, 50, 100), multiple = FALSE, selected = 10)
-        )),
-
+                htmlOutput('selectCompound'),
+                selectInput("ppmerror", "ppm error cut-off",c(1, 5, 10, 15, 20, 50, 100), multiple = FALSE, selected = 10)
+            )),
+        
         # Show a plot of the generated distribution
-        mainPanel(
-           
+        mainPanel(tabsetPanel(
+            tabPanel("stacked", list(
                 column(width = 10,
-            plotOutput("stackedBarChart"),
-            plotOutput("bwplot"),
-            hr(),
-            tableOutput('table'),
-            plotOutput("distPlot"),
-            plotOutput("top3"),
-            plotOutput("xyplot")
-           # plotOutput("barchart")
-           
-            ) 
+                       plotOutput("stackedBarChart"),
+                       plotOutput("top3"),
+                       plotOutput("bwplot"),
+                ))),
+            tabPanel("stacked group", list(
+                column(width = 10,
+                       plotOutput("stackedBarChartGroup")
+                       
+                ))),
+            tabPanel("error", list(
+                column(width = 10,       
+                       plotOutput("distPlot"),
+                       tableOutput('table'),
+                       
+                ))),
+            tabPanel("ms2", list(
+                column(width = 10,             
+                       plotOutput("xyplot")
+                ))),
+            # plotOutput("barchart")
+            tabPanel("matchen in-silico cluster table", list(
+                column(width = 10,   
+                       tableOutput('ThermoUVPD'),
+                )))
+            
+        ) 
         )
     )
 ))
